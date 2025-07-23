59a772697dc6ce9ee9d4b6c04239c9f453b762f9
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-07-23 20:38:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
@@ -1,64 +1,21 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            int k = Integer.parseInt(br.readLine());
-
-            Solution ob = new Solution();
-            int ans = ob.maximumSumSubarray(arr, k);
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
     public int maximumSumSubarray(int[] arr, int k) {
-        int start = 0 , end = k-1;
-        int sum = 0 , maxSum = 0;
-        for(int i=start;i<=end;i++)
+        int i=0,j=0,n=arr.length;
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        while(j<n)
         {
-            sum = sum + arr[i];
-        }
-        maxSum = sum;
-        while(end<arr.length-1)
-        {
-            sum = sum - arr[start];
-            start++;
-            end++;
-            sum = sum + arr[end];
-            maxSum = Math.max(maxSum,sum);
+            sum = sum+arr[j];
+            if(j-i+1 < k) j++;
+            else if(j-i+1 == k)
+            {
+                maxSum = Math.max(maxSum,sum);
+                sum = sum-arr[i];
+                i++;j++;
+            }
         }
         return maxSum;
-        
     }
 }
