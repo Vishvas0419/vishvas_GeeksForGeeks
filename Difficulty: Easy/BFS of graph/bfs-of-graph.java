522b7ea6d7db087d04028a94e7e25cf3c9aaf509
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-11-28 01:39:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,19 +1,18 @@
 class Solution {
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         ArrayList<Integer> ans = new ArrayList<>();
-        Queue<Integer>q = new LinkedList<>();
         int V = adj.size();
         boolean [] vis = new boolean[V];
-        q.offer(0); //source node that is given in the question
-        vis[0] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);//given in q
+        vis[0]=true;
         while(!q.isEmpty())
         {
             int curr = q.poll();
             ans.add(curr);
-            vis[curr] = true;
             for(int nbr : adj.get(curr))
             {
-                if(!vis[nbr])
+                if(!vis[nbr]) 
                 {
                     q.offer(nbr);
                     vis[nbr] = true;
@@ -21,8 +20,6 @@ class Solution {
             }
         }
         return ans;
-        
-        
         
     }
 }
