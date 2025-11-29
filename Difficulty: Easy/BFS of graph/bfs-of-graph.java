584fb72a990f7cae24ff6727b3f3831717c415f8
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-11-29 16:49:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -4,18 +4,18 @@ class Solution {
         int V = adj.size();
         boolean [] vis = new boolean[V];
         Queue<Integer> q = new LinkedList<>();
-        q.offer(0);//given in q
-        vis[0]=true;
+        q.offer(0);
+        vis[0] = true;
         while(!q.isEmpty())
         {
             int curr = q.poll();
             ans.add(curr);
             for(int nbr : adj.get(curr))
             {
-                if(!vis[nbr]) 
+                if(!vis[nbr])
                 {
-                    q.offer(nbr);
                     vis[nbr] = true;
+                    q.offer(nbr);
                 }
             }
         }
