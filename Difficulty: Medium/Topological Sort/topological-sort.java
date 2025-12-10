44a0af436ab82302c524b4f300d1369b00255edb
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-12-11 00:11:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,32 +1,44 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        ArrayList<Integer> result = new ArrayList<>();
-        List<List<Integer>> adj = new ArrayList<>();
-        Stack<Integer>st = new Stack<>();
-        boolean[]vis = new boolean[V];
-        for(int i=0;i<V;i++) adj.add(new ArrayList<>());
-        //build graph in adj list from given 2d edge matrix
-        for(int[]e : edges)
-        {
-            int u = e[0]; int v = e[1];
-            adj.get(u).add(v);
-        }
-        
+        List<List<Integer>>adj = new ArrayList<>();
+        //make indegree array
         for(int i=0;i<V;i++)
         {
-            if(!vis[i]) dfs(i,st,vis,adj);
+            adj.add(new ArrayList<>());
         }
-        int i=0;
-        while(!st.isEmpty()) result.add(st.pop());
-        return result;
-    }
-    static void dfs(int node,Stack<Integer>st,boolean[]vis,List<List<Integer>> adj)
-    {
-        vis[node] = true;
-        for(int nbr : adj.get(node))
+        for(int e[]:edges)
         {
-            if(!vis[nbr]) dfs(nbr,st,vis,adj);
+            int u = e[0];
+            int v = e[1];
+            adj.get(u).add(v);
         }
-        st.push(node);
+        int []indegree = new int[V];
+        for(int i=0;i<V;i++)
+        {
+            for(int nbr : adj.get(i))
+            {
+                indegree[nbr]++;
+            }
+        }
+        Queue<Integer>q  = new LinkedList<>();
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0) q.add(i);
+        }
+        ArrayList<Integer>topo = new ArrayList<>();
+        while(!q.isEmpty())
+        {
+            int node = q.poll();
+            topo.add(node);
+            for(int nbr : adj.get(node))
+            {
+                indegree[nbr]--;
+                if(indegree[nbr]==0)
+                {
+                    q.add(nbr);
+                }
+            }
+        }
+        return topo;
     }
 }
