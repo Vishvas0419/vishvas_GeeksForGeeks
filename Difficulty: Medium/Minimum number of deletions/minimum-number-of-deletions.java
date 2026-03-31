61148655b3634666022706183f82fa7b8ea5c822
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-03-31 11:41:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions/minimum-number-of-deletions.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minDeletions(String s) {
+       return s.length()-lps(s);
+        
+    }
+    private static int lps(String s) {
+        StringBuilder reversedStr = new StringBuilder(s).reverse();
+        return lcs(s,reversedStr.toString());
+    }
+    private static int lcs(String text1, String text2) {
+        int m = text1.length(); int n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
