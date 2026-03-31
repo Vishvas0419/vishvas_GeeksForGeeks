f41c640ba97ded4216a365502db9336df1a10128
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-03-31 11:15:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.java
@@ -0,0 +1,24 @@
+// User function Template for Java
+
+class Solution {
+    public int LongestRepeatingSubsequence(String s) {
+        String x = s;
+        String y = s;
+        int n =s.length();
+        int [][]dp = new int[n+1][n+1];
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(x.charAt(i-1)==y.charAt(j-1) && i!=j)
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+}
