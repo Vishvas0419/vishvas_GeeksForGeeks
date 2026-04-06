691dc60f2e1c014f867da7b7cf32fc56c246052d
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-04-06 18:51:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.java
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.java
@@ -1,30 +1,31 @@
 class Solution {
     static List<Integer> firstNegInt(int arr[], int k) {
         List<Integer> ans = new ArrayList<>();
-        int i=0,j=0;
         Queue<Integer> q = new LinkedList<>();
+        int i=0,j=0;
         while(j<arr.length)
         {
             if(arr[j]<0)
             {
                 q.add(j);
             }
-            if(j-i+1<k) j++;
-            else if(j-i+1==k)
+            if(j-i+1==k)
             {
-                if(q.isEmpty())
+                if(q.isEmpty()) ans.add(0);
+                else
                 {
-                    ans.add(0);
-                }
-                else{
                     ans.add(arr[q.peek()]);
                 }
+                //reverse calculations done earlier before sliding the window 
                 if(!q.isEmpty() && i==q.peek())
                 {
                     q.poll();
                 }
-                i++;j++;
+                
+                i++; //slide the window
             }
+            j++; //if not reavhed the size yet,then increase the window
+            
         }
         return ans;
     }
