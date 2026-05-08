f0a521cf30630279f7dd860951b316cf732e0dee
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-05-09 02:01:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,27 +1,26 @@
 class Solution {
     public int knapsack(int W, int val[], int wt[]) {
+        // code here
         int n = wt.length;
-        int [][]dp = new int[n+1][W+1];
+        int[][]dp = new int[n+1][W+1];
         for(int i=0;i<=n;i++)
         {
             Arrays.fill(dp[i],-1);
         }
         return helper(W,val,wt,n,dp);
     }
-    public int helper(int W,int []val,int []wt,int n,int[][]dp)
+    int helper(int W, int val[], int wt[],int n,int[][]dp)
     {
         if(n==0 || W==0) return 0;
         if(dp[n][W]!=-1) return dp[n][W];
         if(wt[n-1]<=W)
         {
             int pick = val[n-1] + helper(W-wt[n-1],val,wt,n-1,dp);
-            int notPick = helper(W,val,wt,n-1,dp);
+            int notPick =  helper(W,val,wt,n-1,dp);
             return dp[n][W] = Math.max(pick,notPick);
         }
-        else
-        {
-            dp[n][W] = helper(W,val,wt,n-1,dp);
+        else{
+            return dp[n][W] = helper(W,val,wt,n-1,dp);
         }
-        return dp[n][W];
     }
 }
