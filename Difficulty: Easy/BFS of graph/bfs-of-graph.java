9c3caebc48055408d90631b5a22e07dd891d34aa
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-05-28 23:01:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -2,9 +2,9 @@ class Solution {
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         ArrayList<Integer> ans = new ArrayList<>();
         int V = adj.size();
-        boolean [] vis = new boolean[V];
         Queue<Integer> q = new LinkedList<>();
-        q.offer(0);
+        boolean [] vis = new boolean[V];
+        q.add(0);
         vis[0] = true;
         while(!q.isEmpty())
         {
@@ -15,11 +15,10 @@ class Solution {
                 if(!vis[nbr])
                 {
                     vis[nbr] = true;
-                    q.offer(nbr);
+                    q.add(nbr);
                 }
             }
         }
         return ans;
-        
     }
 }
